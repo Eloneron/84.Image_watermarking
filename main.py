fc99c806787facdef5d3d86a452a534c5ac0d39d
robo <lidenbrock5@gmail.com>
2021-12-18 23:48:51 +0100

    Initial release with demo functionality, saves output file.

--- main.py
+++ main.py
@@ -3,50 +3,43 @@ This is an image watermarking app.
 Load an image, watermark and execute.
 
 Release notes:
-Initial release - xx.08.2021
+Initial release - 18.12.2021
+
+No GUI
+Fixed watermark file, size and position, alpha could be better as well
+Fixed source image
+
+Saves output file.
 
 Copyright Robert Pralicz 2021
 """
 
-from tkinter import Tk, Canvas, PhotoImage, Button
-from tkinter.filedialog import askopenfilename
-from PIL import Image, ImageTk
+# imports
 
+from PIL import Image
+import matplotlib.pyplot as plt
 
-BGCOLOR = "#B2B1B9"
-BUTTON = "#141E61"
-FONT = "#252a34"
+# to open the image
+image = Image.open("sample.jpg")
+# this open the photo viewer
+# image.show()
+# plt.imhsow(image)
 
-image, watermark = None, None
+# image watermark
+size = (500, 100)
+watermark = Image.open("watermark_no_bg.png")
+# to keep the aspect ration in intact
+watermark.thumbnail((200, 200), Image.ANTIALIAS)
+# watermark = watermark.putalpha(10)
 
-def get_file():
-    global image
-    file = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
-    PILimage = Image.open(file)
-    if PILimage.width > 800:
-        PILimage.resize(100,200)
-    image = ImageTk.PhotoImage(PILimage)
-
-    return image
-
-def load_img():
-    image = get_file()
-    # file = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
-    canvas.itemconfig(image_id, image=image)
-
-
-window = Tk()
-window.title("Image Watermarking App")
-window.wm_minsize(800, 600)
-window.config(bg=BGCOLOR, padx=10, pady=10)
-
-my_button = Button(text="Load Photo", command=load_img, bg=BGCOLOR, highlightthickness=0)
-my_button.config(padx=10, pady=10)
-my_button.grid(row=2, column=2, columnspan=1)
-
-canvas = Canvas(width=790, height=590, bg=BGCOLOR, highlightthickness=0, )  # highlight... - no border visible
-original_img = PhotoImage(file="sample.png")
-image_id = canvas.create_image(0, 0, anchor='nw', image=original_img)
-canvas.grid(row=1, column=0, columnspan=3)
-
-window.mainloop()
+# add watermark
+copied_image = image.copy()
+# base image
+copied_image.paste(watermark, (30, 500), watermark)
+# pasted the crop image onto the base image
+copied_image.show()
+save = input("do you want to save the image? (y/n) ")
+if save.lower() == 'y':
+    name = input("Enter file name: ")
+    name = name + '.jpg'
+    copied_image.save(name)
